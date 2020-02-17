88c06d60a864a5490da654927f5d8b02472b454d
Krystian Carrillo <krycar12@gmail.com>
2020-02-17 14:14:21 -0700

    added SnakeGame and SnakeGameTester

---
+++ java/src/SnakeGame.java
@@ -0,0 +1,16 @@
+public class SnakeGame {
+    /*constructors
+    default- 2d array with initilizes 1x1 gameboard
+    2nd constructor takes 2d boolean array and x and y positions of head
+    */
+
+    }
+
+    //exastivley- going through every possible way to find it
+    //nested for loop
+    //check how many cells are checked
+
+    //recursivley- using recurssion
+    //stop: finds tail
+    //recursive call: find neighbors and call recursive call on the neighbor until tail is found
+    //check how many cells are checked
