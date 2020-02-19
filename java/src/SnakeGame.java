455217400be2c412d88a920796477aa0bca3c794
Krystian Carrillo <krycar12@gmail.com>
2020-02-19 14:43:53 -0700

    completed constructors and began work on findTailExhaustive

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,10 +1,62 @@
 public class SnakeGame {
-    /*constructors
-    default- 2d array with initilizes 1x1 gameboard
-    2nd constructor takes 2d boolean array and x and y positions of head
-    */
+
+    private boolean [][] game;
+    private int [] headPosition = new int[2];
+    private static int exhaustiveChecks = 0;
+    private static int recursiveChecks = 0;
+
+    public SnakeGame(){
+        game = new boolean [1][1];
+    }
+
+    public SnakeGame(boolean [][] arry, int x, int y){
+        for(int i=0; i<arry.length; i++){
+            for(int j=0; j<arry[i].length; j++){
+                game[i][j] == arry[i][j];
+            }
+        }
+
+        headPosition[0] = x;
+        headPosition[1] = y;
+    }
+
+    public int[] findTailExhaustive(){
+        resetCounters();
+        int tailCounter = 0;
+
+        for(int i=0; i<game.length; i++) {
+            for(int j=0; j<game[i].length; j++){
+                exhaustiveChecks++;
+
+                if(game[i][j] == 1){
+                    tailCounter += 1;
+                }
+            }
+        }
+        //return an array of int size 3 (includes tail position and the length) [tail x, tail y, length]
+    }
+
+    public int[] findTailRecursive(){
+        //return an array of int size 3 (includes tail position and the length) [tail x, tail y, length]
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
 
     }
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getRecursiveChecks(){
+
+    }
+
+    private static int getExhaustiveChecks(){
+
+    }
+}
 
     //exastivley- going through every possible way to find it
     //nested for loop
